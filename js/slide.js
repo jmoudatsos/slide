61f441240f06ed21b2ea7452e7d7628b5a0409e7
Julio Moudatsos <juliomsr098@gmail.com>
2020-09-04 21:31:52 -0300

    sla

--- js/slide.js
+++ js/slide.js
@@ -2,17 +2,35 @@ export default class Slide {
   constructor(slide, warp) {
     this.slide = document.querySelector(slide);
     this.warp = document.querySelector(warp);
+    this.dist = { Final: 0, Startx: 0, Moviment: 0 };
   }
 
   Onstart(event) {
     event.preventDefault();
     this.warp = addEventListener("mousemove", this.Onmove);
+    this.dist.Startx = event.clientX;
   }
-  Onmove() {
-    console.log("mexeu");
+
+  position(clientX) {
+    this.dist.Moviment = (this.dist.Startx - clientX) * 1.6;
+    console.log(`moment:${this.dist.Moviment}`);
+    return this.dist.Final - this.dist.Moviment;
   }
+
+  move(dist) {
+    this.dist.positionmove = dist;
+    console.log(`dist:${dist}`);
+    this.slide.style.transform = `translate3d(${dist}px,0,0)`;
+  }
+
+  Onmove(event) {
+    const fim = this.position(event.clientX);
+    this.move(fim);
+  }
+
   Onend(event) {
     this.warp = removeEventListener("mousemove", this.Onmove);
+    this.dist.Final = this.dist.positionmove;
   }
   Addevent() {
     this.warp = addEventListener("mousedown", this.Onstart);
